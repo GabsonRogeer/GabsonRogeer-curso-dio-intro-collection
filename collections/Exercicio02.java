7a562a9d396d905ed317cf6aaade6761a3f844d1
GabsonRoger <gabsonwona@gmail.com>
2022-01-26 16:45:49 -0300

    add exercicios 01, 02

--- collections/Exercicio02.java
+++ collections/Exercicio02.java
@@ -11,6 +11,60 @@ Se a pessoa responder positivamente a 2 questões ela deve ser classificada como
 "Cúmplice" e 5 como "Assassina". Caso contrário, ele será classificado como "Inocente".
 */
 
-public class Exercicio02 {
+import java.util.*;
 
+public class Exercicio02 {
+    public static void main(String[] args) {
+        List<String> respostas;
+        respostas = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("----------------------------------------");
+        System.out.println("------------ Interrogatorio ------------");
+        System.out.println("----------------------------------------");
+
+        System.out.println("\n------ 1. Telefonou para a vítima? -----");
+        String resposta = scanner.next();
+        respostas.add(resposta.toLowerCase());
+
+        System.out.println("\n----- 2. Esteve no local do crime? -----");
+        resposta = scanner.next();
+        respostas.add(resposta.toLowerCase());
+
+        System.out.println("\n------- 3. Mora perto da vítima? -------");
+        resposta = scanner.next();
+        respostas.add(resposta.toLowerCase());
+
+        System.out.println("\n------- 4. Devia para a vítima? --------");
+        resposta = scanner.next();
+        respostas.add(resposta.toLowerCase());
+
+        System.out.println("\n---- 5. Já trabalhou com a vítima? -----");
+
+        resposta = scanner.next();
+        respostas.add(resposta.toLowerCase());
+
+        System.out.println(respostas);
+
+        int count = 0;
+        for (String resp : respostas) {
+            if (resp.contains("s")) {
+                count++;
+            }
+        }
+
+        switch (count) {
+            case 2 -> System.out.println("\n------------- SUSPEITA!!! -------------");
+            case 3, 4 -> System.out.println("\n------------- CÚMPLICE!!! --------------");
+            case 5 -> System.out.println("\n------------- ASSASSINO!!! ------------");
+            default -> System.out.println("\n-------------   INOCENTE   ------------");
+        }
+
+        System.out.println("----------------------------------------");
+        System.out.println("--------------   FIM DO   --------------");
+        System.out.println("------------ INTERROGATORIO ------------");
+        System.out.println("----------------------------------------");
+
+    }
 }
