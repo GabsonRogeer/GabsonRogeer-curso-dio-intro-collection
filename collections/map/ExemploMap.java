04e40d00fcf06d8a9c0b270f3885d4c3d2117f96
GabsonRoger <gabsonwona@gmail.com>
2022-03-11 23:35:18 -0300

    map exemple

--- collections/map/ExemploMap.java
+++ collections/map/ExemploMap.java
@@ -1,5 +1,6 @@
 package collections.map;
 
+import javax.sound.midi.Soundbank;
 import java.util.*;
 
 public class ExemploMap {
@@ -17,15 +18,94 @@ public class ExemploMap {
     //        HashMap<String, Double> carrosPopulares = new HashMap<>();
     //        Map<String, Double> carrosPopulares2020 = Map.of("gol", 14.4, "uno", 15.6, "mobi", 16.1, "hb20", 14.5, "kwid", 15.6)
     public static void main(String[] args) {
-    
+
+        System.out.println("Crie um dicionário que relacione os modelos e seus respectivos consumos");
          Map<String, Double> carrosPopulares = new HashMap<>(){{
-             put("Gol", 14.4);
+             put("Gol", 16.1);
              put("Uno", 15.6);
              put("Mobi", 16.1);
              put("Hb20", 14.5);
              put("Kwid", 15.6);
          }};
-     
-     
-   }    
+        System.out.println(carrosPopulares.toString());
+
+        System.out.println("Substitiua o consumo do gol por 15,2 km/L:\n ");
+        carrosPopulares.put("Gol", 15.2);
+
+        System.out.println("\n Confira se o modelo tucson está no dicionário: ");
+        carrosPopulares.containsKey("Tucson");
+
+        System.out.println("\nExiba o consumo do uno: " + carrosPopulares.get("Uno"));
+
+        System.out.println("\nExiba os modelos: ");
+        Set<String> modelos = carrosPopulares.keySet();
+        System.out.println(modelos);
+
+        System.out.println("\nExiba os consumos dos carros: ");
+        Collection<Double> consumos = carrosPopulares.values();
+        System.out.println(consumos);
+
+        System.out.println("Exiba o modelo mais econômico e seu consumo: ");
+
+        Double consumoEficiente = Collections.max(carrosPopulares.values());
+        Set<Map.Entry<String, Double>> entries = carrosPopulares.entrySet();
+        String modeloMaisEficiente = "";
+
+        for (Map.Entry<String, Double> entry: entries) {
+            if (entry.getValue().equals(consumoEficiente)) {
+                modeloMaisEficiente = entry.getKey();
+                System.out.println("\nModelo mais efeiciente: " + modeloMaisEficiente + " - " + consumoEficiente);
+            }
+        }
+
+        System.out.println("\nExiba o modelo menos econômico e seu consumo: " );
+
+        Double menosEficiente = Collections.min(carrosPopulares.values());
+        String modeloMenosEficiente = "";
+        for (Map.Entry<String, Double> entry: carrosPopulares.entrySet()) {
+            if(entry.getValue().equals(menosEficiente)) {
+                modeloMenosEficiente = entry.getKey();
+                System.out.println("\nModelo menos eficiente: " + modeloMenosEficiente + " - " + menosEficiente);
+            }
+        }
+
+        Iterator<Double> iterator = carrosPopulares.values().iterator();
+        Double soma = 0d;
+        while(iterator.hasNext()){
+            soma += iterator.next();
+        }
+        System.out.println("\nExiba a soma dos consumos: " + soma);
+
+
+        System.out.println("\nExiba a média dos consumos deste dicionário de carros: " + (soma/carrosPopulares.size()));
+
+        System.out.println(carrosPopulares);
+        System.out.println("\nRemova os modelos com o consumo igual a 15,6 km/l: ");
+        Iterator<Double> iterator1 = carrosPopulares.values().iterator();
+        while(iterator1.hasNext()){
+            if(iterator1.next().equals(15.6)) iterator1.remove();
+        }
+        System.out.println(carrosPopulares);
+
+        System.out.println("\nExiba todos os carros na ordem em que foram informados: ");
+        Map<String, Double> carrosPopulares1 = new LinkedHashMap<>() {{
+            put("gol", 14.4);
+            put("uno", 15.6);
+            put("mobi", 16.1);
+            put("hb20", 14.5);
+            put("kwid", 15.6);
+        }};
+        System.out.println(carrosPopulares1.toString());
+
+        System.out.println("\nExiba o dicionário ordenado pelo modelo: ");
+        Map<String, Double> carrosPopulares2 = new TreeMap<>(carrosPopulares1);
+        System.out.println(carrosPopulares2.toString());
+
+        System.out.println("\nApague o dicionario de carros: ");
+        carrosPopulares.clear();
+
+        System.out.println("\nConfira se o dicionário está vazio: " + carrosPopulares.isEmpty());
+
+   }
+
 }
--- out/production/curso-dio-collections/README.md
+++ out/production/curso-dio-collections/README.md
@@ -1,1 +1,39 @@
  # CURSO INTRODUTÓRIO COLLECTIONS FRAMEWORK JAVA
+
+
+<h2>
+🛑 Pré-requistos
+</h2>
+
+- [x] Java JDK 11
+- [x] IDE IntelliJ
+- [x] Conhecer a sintaxe Java
+- [x] Noção acerca do Paradigma de Programação Orientada a Objeto
+- [x] Disposição para estudar 
+
+<h2> 📚 Ementa</h2>
+
+🔸 <strong> Visão geral:  Java Collections Framework e hierarquia </strong>
+
+🔸 <strong> List </strong><br>
+	  Coleção com iterações ordenadas: List<br>
+	  Conhecendo os métodos<br>
+	  Ordenação da coleção List<br>
+	 
+🔸 <strong> Set </strong><br>
+	  Coleção com singularidade: Set<br>
+	  Conhecendo os métodos<br>
+	  Ordenação da coleção Set<br>
+	 
+🔸 <strong> Map </strong><br>
+	  Coleção de pares: Map<br>
+	  Conhecendo os métodos<br>
+	  Ordenação de Map<br>
+	
+🔸 <strong> Stream API </strong><br>
+	  Visão geral: Classe Anônima - Functional Interface - Lambda  - Method Reference<br>
+	  Stream API<br>
+	  Conhecendo as operações<br>
+   
+   
+   Disponibilizado com ♥ por [cami-la](https://www.linkedin.com/in/cami-la/ "cami-la").
