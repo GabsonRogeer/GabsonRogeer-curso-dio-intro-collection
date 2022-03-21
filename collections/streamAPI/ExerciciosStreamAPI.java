a2e1883162b7fbbde2177ef00a00c5930cddc5d7
GabsonRoger <gabsonwona@gmail.com>
2022-03-20 23:52:20 -0300

    update StreamAPI

--- collections/streamAPI/ExerciciosStreamAPI.java
+++ collections/streamAPI/ExerciciosStreamAPI.java
@@ -4,7 +4,9 @@ import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.DoubleConsumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class ExerciciosStreamAPI {
@@ -20,13 +22,53 @@ public class ExerciciosStreamAPI {
         /*System.out.println("Pegue os 5 primeiros numeros e coloque dentro de um Set: ");
         numerosAleatorios.stream().limit(5).collect(Collectors.toSet()).forEach(System.out::println);*/
 
-        /*System.out.println("Transfome esta lista de String em uma lista de números inteiros.");
+        /*System.out.println("Transfome esta lista de String numa lista de números inteiros.");
         numerosAleatorios.stream()
                 .map(Integer::parseInt).toList()
                 .forEach(System.out::println);*/
 
-        System.out.println();
+        System.out.println("Pegue os números pares e maiores que 2 e coloque-os em uma lista: ");
+        List<Integer> listParesMaiorQue2 = numerosAleatorios.stream()
+                .map(Integer::parseInt)
+                .filter(i -> i %2 == 0 && i > 2 )
+                .collect(Collectors.toList());
+        System.out.println(listParesMaiorQue2);
+
+        System.out.println("Mostrar média dos números: ");
+        numerosAleatorios.stream()
+                .mapToInt(Integer::parseInt)
+                .average()
+                .ifPresent(System.out::println);
+
+        System.out.println("Remova os valores impares: ");
+        List<Integer> numAleatoriosInteger = numerosAleatorios.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        numAleatoriosInteger.removeIf(i -> (i % 2 !=0));
+        System.out.println(numAleatoriosInteger);
+
+        System.out.println("Ignore os 3 primeiros elementos da lista e imprima o restante:");
+        numAleatoriosInteger.stream()
+                .skip(3)
+                .forEach(System.out::println);
 
 
+        long countNumerosUnicos = numAleatoriosInteger.stream()
+                .distinct()
+                .count();
+        System.out.println("Retirando os números repetidos da lista, quantos números ficam? " + countNumerosUnicos);
+
+        System.out.print("\nMostre o menor valor da lista: ");
+        numAleatoriosInteger
+                .stream()
+                .mapToInt(Integer::intValue)
+                .min()
+                .ifPresent(System.out::println);
+
+        System.out.print("Mostre o maior valor da lista: ");
+        numAleatoriosInteger.stream()
+                .mapToInt(Integer::intValue)
+                .max()
+                .ifPresent(System.out::println);
+        }
     }
-}
